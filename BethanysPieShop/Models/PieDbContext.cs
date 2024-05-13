b71ac8a7dce0ec200df39acea3fbe5e3903aaa46
a7iiid <naz131681@gmail.com>
2024-05-13 18:33:24 +0300

    coniction to data base

--- BethanysPieShop/Models/PieDbContext.cs
+++ BethanysPieShop/Models/PieDbContext.cs
@@ -4,5 +4,12 @@ namespace BethanysPieShop.Models
 {
     public class PieDbContext:DbContext
     {
+        public PieDbContext(DbContextOptions<PieDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Pie> pies { get; set; }    
+
     }
+
 }
