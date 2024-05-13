fe26fa8731f28480c6829273f9c4092d1dd2475f
a7iiid <naz131681@gmail.com>
2024-05-13 15:59:06 +0300

    catogery repo

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly PieDbContext _db;
+        public CategoryRepository( PieDbContext pieDbContext)
+        {
+            _db=pieDbContext;
+        }
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return _db.Categories.OrderBy(c=>c.CategoryName);
+            }
+        }
+    }
+}
