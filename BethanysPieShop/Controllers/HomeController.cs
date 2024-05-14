9c0ea4b267a7a330d56683804bbbdad6ebc1cc4e
a7iiid <naz131681@gmail.com>
2024-05-14 10:47:50 +0300

    add home page and ditails view

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.IO.Pipelines;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+        
+    {
+        private readonly IPieRepository _iPieRepository;
+        public HomeController(IPieRepository iPieRepository)
+        {
+            _iPieRepository = iPieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _iPieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(pies); 
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> pisOfTheWeek { get; }
+        public HomeViewModel(IEnumerable<Pie> pies) {
+        pisOfTheWeek = pies;
+        }
+    }
+}
